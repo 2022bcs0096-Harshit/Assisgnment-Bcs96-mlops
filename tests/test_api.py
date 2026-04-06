e901272ba47eb80295b860ee88dc94bab9f05438
2022bcs0096-Harshit <2022bcs0096@gmail.com>
2026-04-06 21:08:27 +0530

    Add unit tests for API and rule engine

---
+++ tests/test_api.py
@@ -0,0 +1,33 @@
+from fastapi.testclient import TestClient
+from src.app import app
+
+client = TestClient(app)
+
+
+def test_health_check():
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json()["status"] == "service running"
+
+
+def test_predict_risk_valid_customer():
+
+    response = client.post(
+        "/predict-risk",
+        json={"customer_id": "7590-VHVEG"}
+    )
+
+    assert response.status_code == 200
+    assert "risk_category" in response.json()
+
+
+def test_predict_risk_invalid_customer():
+
+    response = client.post(
+        "/predict-risk",
+        json={"customer_id": "INVALID-ID"}
+    )
+
+    assert response.status_code == 404
